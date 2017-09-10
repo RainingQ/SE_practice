2ee82bd8802603b5ff9e26090ce594226ca8b86c
RainingQ <275922122@qq.com>
2017-09-10 23:09:37 +0800

    Alter

--- psp_sudoku/sudoku/DisplaySudo.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef __DISPLAYSUDO_H_
-#define __DISPLAYSUDO_H_
-#include<iostream>
-using namespace std;
-
-void display(int(*sudo)[9]);
-
-#endif
--- psp_sudoku/sudoku/SudoGenerator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef __SUDOGENERATOR_H_
-#define __SUDOGENERATOR_H_
-#include<iostream>
-using namespace std;
-
-void SudoGenerator(int N, int(*sudo)[9]);
-
-#endif
